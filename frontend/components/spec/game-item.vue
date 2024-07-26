088ad75fd05fef213908b5be692f3c7c08a40d21
Tieng <vinh.tieng@dudaji.vn>
2024-07-26 14:58:16 +0700

    Update game-item and index styles

--- frontend/components/spec/game-item.vue
+++ frontend/components/spec/game-item.vue
@@ -1,7 +1,7 @@
 <template>
   <NuxtLink
     :to="game.link"
-    class="flex flex-col items-center justify-start md:w-[20%] md:hover:bg-white md:hover:shadow-2xl p-3 rounded-[48px]"
+    class="flex flex-col items-center justify-start md:w-[20%] md:hover:bg-white md:hover:shadow-2xl p-3 rounded-[48px] group-hover:opacity-10 hover:!opacity-100 hover:!scale-110 transition-all duration-300"
   >
     <img :src="game.thumbnail" :alt="game.name" class="w-full aspect-square" />
     <p class="text-lg font-semibold flex-1 -mt-2">{{ game.name }}</p>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -2,7 +2,7 @@
   <div>
     <h1 class="pt-10 text-lg text-center opacity-50">Các trò chơi đang hoạt động</h1>
     <div
-      class="w-full md:flex md:items-stretch md:justify-center gap-5 mt-10 grid grid-cols-2"
+      class="w-full md:flex md:items-stretch md:justify-center gap-5 mt-10 grid grid-cols-2 group"
     >
       <SpecGameItem v-for="game in GAMES" :game="game" :key="game.link" />
     </div>
